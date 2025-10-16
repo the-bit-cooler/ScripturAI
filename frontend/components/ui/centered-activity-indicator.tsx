eb2cf219fe66777fc47bc6208eddde1673ebedda
The Bit Cooler <thebitcooler@icloud.com>
2025-10-16 10:33:59 +0000

    fix: support floating title for centered activity indicator

--- frontend/components/ui/centered-activity-indicator.tsx
+++ frontend/components/ui/centered-activity-indicator.tsx
@@ -1,16 +1,19 @@
 import { ActivityIndicator, ColorValue, StyleSheet, View } from 'react-native';
 
 import { useThemeColor } from '@/hooks/use-theme-color';
+import { ThemedText } from '../themed-text';
 
 type CenteredActivityIndicatorProps = {
   size?: 'small' | 'large' | number;
   color?: ColorValue;
+  hint?: string;
 };
 
-export function CenteredActivityIndicator({ size, color }: CenteredActivityIndicatorProps) {  
+export function CenteredActivityIndicator({ size, color, hint }: CenteredActivityIndicatorProps) {
   const activityColor = useThemeColor({}, 'tint');
   return (
     <View style={styles.activityIndicatorContainer}>
+      {hint && <ThemedText type='subtitle' style={{ marginBottom: 20 }}>{hint}</ThemedText>}
       <ActivityIndicator size={size} color={color ?? activityColor} />
     </View>
   );
