419f8e2063c87cda0d73b025579bff5ece993e53
The Bit Cooler <thebitcooler@icloud.com>
2025-10-20 01:25:28 +0000

    fix: overhauled settings modal to use native pickers and AppPreferencesProvider

--- frontend/app/settings.tsx
+++ frontend/app/settings.tsx
@@ -1,133 +1,24 @@
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Application from 'expo-application';
-import { useEffect, useState } from "react";
-import { Alert, Button, ScrollView, StyleSheet, Switch, TouchableOpacity, View } from "react-native";
+import { Alert, Button, ScrollView, StyleSheet, Switch, View } from "react-native";
+import { Picker } from '@react-native-picker/picker';
 
 import { AppTheme, useAppTheme } from "@/hooks/use-app-theme-provider";
+import { useAppPreferences } from "@/hooks/use-app-preferences-provider";
 import { useThemeColor } from "@/hooks/use-theme-color";
 
 import { Colors } from "@/constants/theme";
-import { UserPreferences } from "@/constants/user-preferences";
-import { AiModes, AiModeValues } from "@/constants/ai-modes";
+import { AiModeValues } from "@/constants/ai-modes";
 
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { HorizontalThemedSeparator } from "@/components/ui/themed-separator";
 
-type AppearanceSectionProps = {
-  appTheme: AppTheme;
-  setAppTheme: (theme: AppTheme) => void
-  color: string;
-  selectedColor: string;
-}
-
-const AppearanceSection = ({ appTheme, setAppTheme, color, selectedColor }: AppearanceSectionProps) => {
-  const themeOptions = ["light", "dark", "sepia", "system"] as const;
-
-  return (
-    <>
-      <ThemedText type="subtitle" style={[styles.header, { color }]}>
-        Appearance
-      </ThemedText>
-      {themeOptions.map((option) => {
-        const isSelected = appTheme === option;
-        return (
-          <TouchableOpacity
-            key={option}
-            style={[styles.optionButton, { borderColor: isSelected ? selectedColor : "transparent" }]}
-            onPress={() => setAppTheme(option)}
-          >
-            <ThemedText style={{ color: isSelected ? selectedColor : color }}>
-              {option.charAt(0).toUpperCase() + option.slice(1)}
-            </ThemedText>
-          </TouchableOpacity>
-        );
-      })}
-    </>
-  );
-};
-
-type AiModeSectionProps = {
-  color: string;
-  selectedColor: string;
-}
-
-const AiModeSection = ({ color, selectedColor }: AiModeSectionProps) => {
-  const [mode, setMode] = useState(AiModes.devotional);
-  const [allowThinkingSound, setAllowThinkingSound] = useState(true);
-
-  useEffect(() => {
-    const loadPreference = async () => {
-      try {
-        const storedMode = await AsyncStorage.getItem(UserPreferences.ai_mode);
-        if (storedMode) {
-          setMode(storedMode);
-        }
-        const storedThinkingSound = await AsyncStorage.getItem(UserPreferences.ai_thinking_sound);
-        if (storedThinkingSound === "false") {
-          setAllowThinkingSound(false);
-        }
-      } catch (err) {
-        console.error('Error loading AI mode preference:', err);
-      }
-    };
-
-    loadPreference();
-  }, [mode, allowThinkingSound]);
-
-  return (
-    <View style={{ marginTop: 10 }}>
-      <ThemedText type="subtitle" style={[styles.header, { color }]}>
-        AI Mode
-      </ThemedText>
-      {AiModeValues.map((option) => {
-        const isSelected = mode === option;
-        return (
-          <TouchableOpacity
-            key={option}
-            style={[styles.optionButton, { borderColor: isSelected ? selectedColor : "transparent" }]}
-            onPress={async () => {
-              try {
-                await AsyncStorage.setItem(UserPreferences.ai_mode, option);
-                setMode(option);
-              } catch (err) {
-                Alert.alert("Error", "Something went wrong! Pleas try again later.");
-                console.error('Error storing AI mode preference: ', err);
-              }
-            }}
-          >
-            <ThemedText style={{ color: isSelected ? selectedColor : color }}>
-              {option.charAt(0).toUpperCase() + option.slice(1)}
-            </ThemedText>
-          </TouchableOpacity>
-        );
-      })}
-      <View style={styles.soundToggleContainer}>
-        <ThemedText style={styles.soundToggleLabel}>Enable Thinking Sound</ThemedText>
-        <Switch
-          value={allowThinkingSound}
-          onValueChange={async (value) => {
-            try {
-              await AsyncStorage.setItem(UserPreferences.ai_thinking_sound, value ? "true" : "false");
-              setAllowThinkingSound(value);
-            } catch (err) {
-              console.warn("Error saving thinking sound preference", err);
-            }
-          }}
-          // trackColor={{ false: "#81b0ff", true: "#81b0ff" }}
-          // thumbColor={allowThinkingSound ? "" : "red"}
-        />
-      </View>
-    </View>
-  );
-};
-
 export default function SettingsScreen() {
   const { theme, setTheme } = useAppTheme();
   const backgroundColor = useThemeColor({}, "cardBackground")
   const textColor = useThemeColor({}, "text");
-  const tintColor = useThemeColor({}, "tint");
 
 
   const clearStorage = async () => {
@@ -166,14 +57,14 @@ export default function SettingsScreen() {
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <AppearanceSection
           appTheme={theme}
+          headerColor={textColor}
+          pickerColor={textColor}
           setAppTheme={setTheme}
-          color={textColor}
-          selectedColor={tintColor}
         />
-        <HorizontalThemedSeparator />
+        <HorizontalThemedSeparator marginVertical={0} />
         <AiModeSection
-          color={textColor}
-          selectedColor={tintColor}
+          headerColor={textColor}
+          pickerColor={textColor}
         />
         <HorizontalThemedSeparator />
         <View style={{ marginTop: 10 }}>
@@ -187,24 +78,82 @@ export default function SettingsScreen() {
   );
 }
 
+type AppearanceSectionProps = {
+  appTheme: AppTheme;
+  headerColor: string;
+  pickerColor: string;
+  setAppTheme: (theme: AppTheme) => void
+}
+
+const AppearanceSection = ({ appTheme, headerColor, pickerColor, setAppTheme }: AppearanceSectionProps) => {
+  const themeOptions = ["light", "dark", "sepia", "system"] as const;
+  const themeOptionLabels = ["Light Mode", "Dark Mode", "Reading Mode", "System Default"] as const;
+
+  return (
+    <View>
+      <ThemedText type="subtitle" style={{ color: headerColor }}>
+        Appearance
+      </ThemedText>
+      <Picker
+        selectedValue={appTheme}
+        style={{ marginVertical: -10 }}
+        itemStyle={{ borderRadius: 200, color: pickerColor, backgroundColor: 'transparent', fontWeight: 'bold' }}
+        onValueChange={(theme) => setAppTheme(theme)}
+      >
+        {themeOptions.map((option, index) => {
+          return (
+            <Picker.Item key={option} label={themeOptionLabels[index]} value={option} />
+          );
+        })}
+      </Picker>
+    </View>
+  );
+};
+
+type AiModeSectionProps = {
+  headerColor: string;
+  pickerColor: string;
+}
+
+const AiModeSection = ({ headerColor, pickerColor }: AiModeSectionProps) => {
+  const { aiMode, setAiMode, allowThinkingSound, setAllowThinkingSound } = useAppPreferences();
+
+  return (
+    <>
+      <View style={{ marginTop: 20 }}>
+        <ThemedText type="subtitle" style={{ color: headerColor, marginBottom: 0 }}>
+          AI Mode
+        </ThemedText>
+        <Picker
+          selectedValue={aiMode}
+          onValueChange={(mode) => setAiMode(mode)}
+          style={{ marginTop: -40 }}
+          itemStyle={{ borderRadius: 200, color: pickerColor, backgroundColor: 'transparent', fontWeight: 'bold' }}
+        >
+          {AiModeValues.map((option) => {
+            return (
+              <Picker.Item key={option} label={option.charAt(0).toUpperCase() + option.slice(1)} value={option} />
+            );
+          })}
+        </Picker>
+      </View>
+      <View style={styles.soundToggleContainer}>
+        <ThemedText style={styles.soundToggleLabel}>Enable Thinking Sound</ThemedText>
+        <Switch
+          value={allowThinkingSound}
+          onValueChange={(value) => setAllowThinkingSound(value)}
+        />
+      </View>
+    </>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   scrollContainer: {
     padding: 20,
-  },
-  header: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 16,
-  },
-  optionButton: {
-    paddingVertical: 12,
-    paddingHorizontal: 16,
-    borderWidth: 2,
-    borderRadius: 8,
-    marginVertical: 6,
   },
   soundToggleContainer: {
     flexDirection: "row",
