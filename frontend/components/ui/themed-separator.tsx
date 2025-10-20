4f48fd78463263cf224a7bae4e8fb2954e6130cd
The Bit Cooler <thebitcooler@icloud.com>
2025-10-20 00:36:03 +0000

    fix: removed unused vertical themed separator and allow margin vertical to be set directly on horizontal one

--- frontend/components/ui/themed-separator.tsx
+++ frontend/components/ui/themed-separator.tsx
@@ -1,24 +1,7 @@
 import { useThemeColor } from "@/hooks/use-theme-color";
 import { View } from "react-native";
 
-export function VerticalThemedSeparator({ style }: { style?: any }) {
-  const borderColor = useThemeColor({}, "border");
-  return (
-    <View
-      style={[
-        {
-          width: 1.5,
-          backgroundColor: borderColor + '80', // 50% opacity
-          marginVertical: 18,
-          borderRadius: 1,
-        },
-        style,
-      ]}
-    />
-  );
-}
-
-export function HorizontalThemedSeparator({ style }: { style?: any }) {
+export function HorizontalThemedSeparator({marginVertical}: {marginVertical?: number}) {
   const borderColor = useThemeColor({}, "border");
   return (
     <View
@@ -26,10 +9,9 @@ export function HorizontalThemedSeparator({ style }: { style?: any }) {
         {
           height: 1.5,
           backgroundColor: borderColor + '80', // 50% opacity
-          marginVertical: 18,
+          marginVertical: marginVertical ?? 10,
           borderRadius: 1,
-        },
-        style,
+        }
       ]}
     />
   );
